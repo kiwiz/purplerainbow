e2c176fe6b0b5a38835bdb08f4dc20c24ef15262
X <X>
2015-04-18 16:52:13 -0400

    Add estimize endpoint

---
+++ temp/estimize_connector.py
@@ -0,0 +1,25 @@
+from estimize import *
+
+rd_keys = ['consensus_revenue_estimate', 'consensus_eps_estimate', 'wallstreet_eps_estimate', 'wallstreet_revenue_estimate']
+
+def get_analysis_estimate_data(ticker):
+    releases = request_releases_for_company("AAPL")
+    ret = []
+    for release in releases:
+#        if release['fiscal_year'] >= 
+        for k in rd_keys:
+            del release[k]
+
+        details = request_release_consensus(release['id'])
+        if not (details['wallstreet']['eps'] or details['estimize']['eps'] or details['wallstreet']['revenue'] or details['estimize']['revenue']):
+            continue
+
+        for a in ['estimize', 'wallstreet']:
+            for b in details[a].keys():
+                if 'revisions' in details[a][b]:
+                    del details[a][b]['revisions']
+        ret.append(release.update(details))
+
+    return ret
+
+get_analysis_estimate_data('APPL')
