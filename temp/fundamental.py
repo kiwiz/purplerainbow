ea0a700ff22f7750a889cccfda437a2f5d70173f
X <X>
2015-04-19 08:00:06 -0400

    get market data function

--- temp/fundamental.py
+++ temp/fundamental.py
@@ -1,5 +1,50 @@
 import requests
 import json
+import datetime
+
+def get_market_data(ticker):
+
+    agg      = []
+    currTime = datetime.datetime.now()
+    currDate = currTime.date()
+    apiKey   = 'huFif6YjwJKCfmKXIZn1E1xy'
+
+    endDelta  = datetime.timedelta( 365 )
+    oneDay    = datetime.timedelta( 1 )
+    oneWk     = datetime.timedelta( 7 )
+    Dt        = currDate - endDelta
+
+    while Dt <= currDate:
+        query = 'https://api.tagnifi.com/markets?relative_period=0&company={0}&date={1}'.format( ticker, '{0}-{1}-{2}'.format(Dt.year, Dt.strftime("%m"), Dt.strftime("%d")) )
+        
+        r = requests.get( query, auth=( apiKey, '' ), verify=False )
+
+        try:
+            j = r.json()
+        except ValueError:
+            err = True
+    
+        vol   = j.get( 'markets' )[0].get( 'prices' )[0].get( 'volume' )
+        close = j.get( 'markets' )[0].get( 'prices' )[0].get( 'close' )
+
+        structure = {
+            'ticker' : ticker,
+            'date'   : Dt
+        }
+        structure[ 'vol'   ] = vol
+        structure[ 'close' ] = close
+
+        Dt = Dt + oneWk
+#        print( currDate )
+
+        agg.append( structure )
+
+    return agg
+
+#print( datetime.datetime.now() )
+#d = get_market_data('msft')
+#print( d )
+#print( datetime.datetime.now() )
 
 def get_fundamental_data(ticker):
 
@@ -12,7 +57,7 @@ def get_fundamental_data(ticker):
         'TotalAssets':'total_assets',
     }
 
-    currYear = 2015
+    currYear = datetime.datetime.now().year
     apiKey   = 'huFif6YjwJKCfmKXIZn1E1xy'
     agg = []
 
@@ -24,8 +69,8 @@ def get_fundamental_data(ticker):
 
         for q in range( 1, 5 ):
 
-            map = {}
-            err = False
+            structure = {}
+            err       = False
 
             for f in fields:
                 if q < 4:
@@ -51,13 +96,13 @@ def get_fundamental_data(ticker):
                 elif f in cumFields.keys():
                     cumFields[ f ] += v
 
-                map[ n ] = v
+                structure[ n ] = v
 
             if err == False:
-                map[ 'ticker' ] = ticker
-                map[ 'fiscal_year' ] = y
-                map[ 'fiscal_qtr'  ] = q
-                agg.append( map )
+                structure[ 'ticker' ] = ticker
+                structure[ 'fiscal_year' ] = y
+                structure[ 'fiscal_qtr'  ] = q
+                agg.append( structure )
 
     return agg
 
