1aaea6988942295e6956ad7bce1cea1f26df5101
X <X>
2015-04-19 09:44:47 -0400

    use limit in market data query

--- temp/fundamental.py
+++ temp/fundamental.py
@@ -9,13 +9,13 @@ def get_market_data(ticker):
     currDate = currTime.date()
     apiKey   = 'huFif6YjwJKCfmKXIZn1E1xy'
 
-    endDelta  = datetime.timedelta( 365 )
-    oneDay    = datetime.timedelta( 1 )
-    oneWk     = datetime.timedelta( 7 )
+    endDelta  = datetime.timedelta( 365 * 3 )
     Dt        = currDate - endDelta
+    gap       = 25
+    gapPeriod = datetime.timedelta( gap )
 
     while Dt <= currDate:
-        query = 'https://api.tagnifi.com/markets?relative_period=0&company={0}&date={1}'.format( ticker, '{0}-{1}-{2}'.format(Dt.year, Dt.strftime("%m"), Dt.strftime("%d")) )
+        query = 'https://api.tagnifi.com/markets?relative_period=0&company={0}&limit={1}&date={2}'.format( ticker, gap, '{0}-{1}-{2}'.format(Dt.year, Dt.strftime("%m"), Dt.strftime("%d")) )
         
         r = requests.get( query, auth=( apiKey, '' ), verify=False )
 
@@ -23,28 +23,25 @@ def get_market_data(ticker):
             j = r.json()
         except ValueError:
             err = True
-    
-        vol   = j.get( 'markets' )[0].get( 'prices' )[0].get( 'volume' )
-        close = j.get( 'markets' )[0].get( 'prices' )[0].get( 'close' )
 
-        structure = {
-            'ticker' : ticker,
-            'date'   : Dt
-        }
-        structure[ 'vol'   ] = vol
-        structure[ 'close' ] = close
+        data   = j.get( 'markets' )[0].get( 'prices' )
+        
+        for i in range( 0, gap ):
+            structure = {
+                'ticker' : ticker,
+                'date'   : data[ i ].get( 'date' )
+            }
+            structure[ 'volume' ] = data[ i ].get( 'volume' )
+            structure[ 'close'  ] = data[ i ].get( 'close' )
+            
+            agg.append( structure )
 
-        Dt = Dt + oneWk
-#        print( currDate )
+        Dt = Dt + gapPeriod
 
-        agg.append( structure )
+    agg = sorted( agg, key = lambda x: x[ 'date' ] )
 
     return agg
 
-#print( datetime.datetime.now() )
-#d = get_market_data('msft')
-#print( d )
-#print( datetime.datetime.now() )
 
 def get_fundamental_data(ticker):
 
